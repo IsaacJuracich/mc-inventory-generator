90800f11843ff1b44d884995f2d2f47aa1256113
IsaacJuracich <93289395+IsaacJuracich@users.noreply.github.com>
2022-02-14 12:08:16 -0800

    s

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import express from 'express';
-import swaggerUI from 'swagger-ui-express';
-import router from './handlers';
-import swaggerDocument from '../swagger.json';
-
-const app = express();
-app.use('/', router());
-app.use('/', swaggerUI.serve);
-app.get('/', swaggerUI.setup(swaggerDocument));
-
-app.listen(+process.env.PORT || 3000);
+export default async (options) => {
+    const app = options.express()
+}
